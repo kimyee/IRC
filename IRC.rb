17438b901e8f07bc7b2d422fa82f8a976b5ae635
kimyee <kimyee.16@gmail.com>
2013-08-24 15:54:07 -0400

    first commit

---
+++ IRC.rb
@@ -0,0 +1,36 @@
+require "socket"
+require "pry"
+
+server = "chat.freenode.net"
+port = "6667"
+nick = "MomooBot"
+channel = "#bitmaker"
+greeting_prefix = "privmsg #bitmaker :"
+greetings = [" "]
+
+irc_server = TCPSocket.open(server, port)
+
+irc_server.puts "USER bhellobot 0 * BHelloBot"
+irc_server.puts "NICK #{nick}"
+irc_server.puts "JOIN #{channel}"
+
+until irc_server.eof? do
+	msg = irc_server.gets.downcase
+	#binding.pry
+	puts msg
+	
+	wasGreeted = false
+  	greetings.each do |g|
+		wasGreeted = true if msg.include? g
+  	end
+
+ 
+
+	if msg.include? greeting_prefix and wasGreeted
+		response = "what..."
+		msg = msg.split("privmsg #bitmaker :")
+		msg = msg[1]
+
+		irc_server.puts "PRIVMSG #{channel} :#{msg}"
+	end
+end
